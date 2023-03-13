d8f1d1b761f49ee42eab18d66d920f7b95817f58
HaruunZulkifl <harunzulkifl@gmail.com>
2023-03-13 18:19:40 +0500

    Repositories changed to jdbc tamplate

---
+++ src/main/java/org/example/util/ScannerUtil2.java
@@ -0,0 +1,8 @@
+package org.example.util;
+
+import java.util.Scanner;
+
+public class ScannerUtil2 {
+    public static Scanner SCANNER_INT = new Scanner(System.in);
+    public static Scanner SCANNER_STR = new Scanner(System.in);
+}
